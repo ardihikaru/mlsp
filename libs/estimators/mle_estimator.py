94550ff86c69857f59f9e7f00f80416d2b14859c
ardihikaru <ardihikaru3@gmail.com>
2019-10-03 11:30:50 +0800

    updates: - finished calculating MLE dataset

--- libs/estimators/mle_estimator.py
+++ libs/estimators/mle_estimator.py
@@ -1,11 +1,15 @@
 # from math import exp
 import numpy as np
 # from .mse_estimator import MSE
+from libs.distributions.exp_dist import ExpDist
 
 class MLE():
-    def __init__(self, lam, x):
-        self.y = lam
-        self.x = x
+    # def __init__(self, lam, x):
+    def __init__(self, **expo_opt):
+        # self.y = lam
+        # self.x = x
+        self.expo = ExpDist(**expo_opt)
+
     # def __calc_y(self, x, lam):
     #     return [lam * exp(-lam * xi) for xi in x]
 
@@ -15,11 +19,20 @@ class MLE():
         self.__calc_yhat()
         # self.__calc_mse()
 
-    def get_y(self):
-        return self.y
+    def multiple_run(self, n):
+        self.array_yhat = np.empty(shape=[n])
+        for i in range(1, n):
+            self.run()
+            self.array_yhat[i] = self.get_yhat()
+
+    def get_array_yhat(self):
+        return self.array_yhat
+
+    # def get_y(self):
+    #     return self.y
 
     def __calc_yhat(self):
-        self.yhat = 1 / np.average(self.x)
+        self.yhat = 1 / np.average(self.expo.get_dataset())
 
     def get_yhat(self):
         return self.yhat
