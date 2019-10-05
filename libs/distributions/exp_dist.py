89bae5c5a88980344621e6c8d4d5c959bdb3dd1d
Ardihikaru <ardihikaru3@gmail.com>
2019-10-06 00:55:49 +0800

    updates: - finished hw1

--- libs/distributions/exp_dist.py
+++ libs/distributions/exp_dist.py
@@ -11,6 +11,10 @@ class ExpDist():
         self.lam = kwargs["lam"]
         self.st_range = kwargs["st_range"]
 
+        # optional: used only in hw1c.ipynb and hw2d.ipnyb
+        self.alpha = kwargs["alpha"]
+        self.beta = kwargs["beta"]
+
     def get_size(self):
         return self.size
 
@@ -27,11 +31,23 @@ class ExpDist():
         beta = 1/ self.lam
         self.rand_exp = np.random.exponential(beta, self.size)
 
-    def regenerate_dataset(self):
+    def regenerate_dataset(self, new_alpha=None, n_size=None):
+        if new_alpha is not None:
+            self.alpha = new_alpha
+        if n_size is not None:
+            self.size = n_size
         self.__generate_dataset()
 
     def get_dataset(self):
         return self.rand_exp
+
+    # optional usage
+    def get_alpha(self):
+        return self.alpha
+
+    # optional usage
+    def get_beta(self):
+        return self.beta
 
     def plot_dataset(self):
         plt.hist(self.rand_exp, density=True, bins=self.size * 2, lw=100, alpha=.9)
--- libs/estimators/map_estimator.py
+++ libs/estimators/map_estimator.py
@@ -2,24 +2,30 @@ import numpy as np
 from libs.distributions.gamma_dist import GammaDist
 
 class MAP():
-    def __init__(self, **input_variables):
-        self.gamma_dist = GammaDist(**input_variables)
+    def __init__(self, ext_dataset=None, **input_variables):
+        if ext_dataset is not None:
+            self.data = ext_dataset
+        else:
+            self.data = GammaDist(**input_variables) # default: Gamma Dataset
 
     def __inits_variables(self):
-        self.n = self.gamma_dist.get_size()
-        self.x = self.gamma_dist.get_dataset()
+        self.n = self.data.get_size()
+        self.x = self.data.get_dataset()
         self.x_sum = np.sum(self.x)
-        self.alpha = self.gamma_dist.get_alpha()
-        self.beta = self.gamma_dist.get_beta()
+        self.alpha = self.data.get_alpha()
+        self.beta = self.data.get_beta()
 
     def run(self):
         self.__inits_variables()
         self.__calc_yhat()
 
-    def multiple_run(self, n_retry, new_alpha=None):
+    def multiple_run(self, n_retry, new_alpha=None, data=None):
         self.array_yhat = np.empty(shape=[n_retry])
         for i in range(0, n_retry):
-            self.gamma_dist.regenerate_dataset(new_alpha)
+            if data is not None: # data regeneration is performed before assigned here
+                self.data = data
+            else:
+                self.data.regenerate_dataset(new_alpha)
             self.run()
             self.array_yhat[i] = self.get_yhat()
 
@@ -33,4 +39,4 @@ class MAP():
         return self.yhat
 
     def plot(self):
-        self.gamma_dist.plot_dataset()
+        self.data.plot_dataset()
