6eed15c684a51c3726db2d0875f7552b58f273a3
Ardihikaru <ardihikaru3@gmail.com>
2019-10-06 00:55:49 +0800

    updates: - finished hw1

--- libs/estimators/mle_estimator.py
+++ libs/estimators/mle_estimator.py
@@ -2,16 +2,22 @@ import numpy as np
 from libs.distributions.exp_dist import ExpDist
 
 class MLE():
-    def __init__(self, **input_variables):
-        self.exp_dist = ExpDist(**input_variables)
+    def __init__(self, ext_dataset=None, **input_variables):
+        if ext_dataset is not None:
+            self.data = ext_dataset
+        else:
+            self.data = ExpDist(**input_variables) # default: Exponential Dataset
 
     def run(self):
         self.__calc_yhat()
 
-    def multiple_run(self, n_retry):
+    def multiple_run(self, n_retry, data=None):
         self.array_yhat = np.empty(shape=[n_retry])
         for i in range(0, n_retry):
-            self.exp_dist.regenerate_dataset()
+            if data is not None: # data regeneration is performed before assigned here
+                self.data = data
+            else:
+                self.data.regenerate_dataset()
             self.run()
             self.array_yhat[i] = self.get_yhat()
 
@@ -19,10 +25,10 @@ class MLE():
         return self.array_yhat
 
     def __calc_yhat(self):
-        self.yhat = 1 / np.average(self.exp_dist.get_dataset())
+        self.yhat = 1 / np.average(self.data.get_dataset())
 
     def get_yhat(self):
         return self.yhat
 
     def plot(self):
-        self.exp_dist.plot_dataset()
+        self.data.plot_dataset()
