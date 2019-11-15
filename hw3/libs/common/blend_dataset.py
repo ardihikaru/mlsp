00b80e1601d6f01bf57e0f36e507ac7bf8a205d6
ardihikaru <ardihikaru3@gmail.com>
2019-11-15 13:52:29 +0800

    updates: - finished hw3b with PCA, ICA, and NMF

--- hw3/libs/common/blend_dataset.py
+++ hw3/libs/common/blend_dataset.py
@@ -2,12 +2,17 @@ import numpy as np
 import glob
 from matplotlib.image import imread
 import matplotlib.pyplot as plt
+import pandas as pd
 
 class BlendImgDataset():
-    def __init__(self, o_img_size):
+    def __init__(self, o_img_size, df_dataset=False, shape=None, N=None, p=None, all=False):
         self.total_img = 0
         self.o_img_size = o_img_size
         self.img_data = [] # initial data matrix
+
+        self.shape = shape  # origianl image size
+        self.N = N  # image sample size
+        self.p = p  # = 784
 
         # self.source_img_path = "./hw3/blend_20/"
         # self.img_ext = ".jpg"
@@ -29,12 +34,33 @@ class BlendImgDataset():
         # self.img_ext = ".png"
         # self.expected_type = 2
 
-        self.source_img_path = "./hw3/blend_small/"
-        self.img_ext = ".jpg"
-        self.expected_type = 4
+        if all:
+            self.source_img_path = "./hw3/mixture_dataset(0147)/"
+            self.img_ext = ".jpg"
+            self.expected_type = 4
+        else:
+            self.source_img_path = "./hw3/blend_small/"
+            self.img_ext = ".jpg"
+            self.expected_type = 4
 
-        self.__load_default_imgs()
-        # self.check_signal_cor()
+        if df_dataset:
+            self.__load_default_imgs_df(p)
+        else:
+            self.__load_default_imgs()
+            # self.check_signal_cor()
+
+    def __load_default_imgs_df(self, p):
+        X = []
+        indices = []
+        img_data = self.source_img_path + "*" + self.img_ext
+        for filename in glob.glob(img_data):
+            index = filename[3:-4]
+            self.total_img += 1
+            print(" reading dataset, index = ", index)
+            img = plt.imread(filename)   # notice that the input file order is random!!!!!!!
+            X.append(img.ravel())
+            indices.append(index)
+            self.img_data = pd.DataFrame(X, columns=["V" + str(p) for p in range(p)], index=indices)
 
     def __load_default_imgs(self):
         # i = 0
