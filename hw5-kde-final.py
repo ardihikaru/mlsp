17393cff70eed78ac43c6bb75e63dfc176d9d6bc
ardihikaru <ardihikaru3@gmail.com>
2020-01-06 00:12:47 +0800

    finished hw5

--- hw5-kde-final.py
+++ hw5-kde-final.py
@@ -1,26 +1,36 @@
 from hw5.libs.common.dataset import Dataset
-from hw5.libs.common.util import int_to_tuple, save_to_csv
+from hw5.libs.common.util import step_size_to_tuple, save_to_csv
 from hw5.libs.algo.KDEClassifier import KDEClassifier
 from datetime import datetime
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 if __name__ == '__main__':
     # dataset = Dataset(train_data=1000, test_data=100)
     # dataset = Dataset(train_data=40, test_data=10)
     dataset = Dataset(train_data=80, test_data=20)
+    # dataset = Dataset(train_data=800, test_data=200)
     # dataset = Dataset()
     X_train, Y_train, X_test, Y_test = dataset.get_dataset()
     train_scores = []
     test_scores = []
-    # exec_times = [0]
-    # best_test_score = 0
-    # stored_accuracy = [0, 0] # [<acc>, <k-th>
+    exec_times = []
+    best_test_score = 0
+    stored_accuracy = [0, 0] # [<acc>, <k-th>
+
+    # Disable this PCA to obtain the best result!
+    # Evaluate: Try performing PCA first, and check the results
+    # Result: Reducing PCA is not recommended, since it will dramatically reduce the accuracy.
+    # Dimensional reduction
+    # pca = PCA(n_components=16, whiten=False)
+    # X_train = pca.fit_transform(X_train)
+    # X_test = pca.fit_transform(X_test)
 
     # Define value of bandwidth
     K = 50 # Must be positive and higher than 0 (ZERO)
-    step_size = 0.05
     step_init = 0.05
-    ks = int_to_tuple(K)  # used to plot the results
+    step_size = 0.05
+    ks = step_size_to_tuple(K, step_init, step_size)  # used to plot the results
     # next:
     # 1. bikin array sesuai step size, contoh: ks = (0.05, 0.1, 0.15, 0.2, ...) sampai sejumlah K
     # 2. looping sejumlah K
@@ -30,20 +40,21 @@ if __name__ == '__main__':
 
     # Start KDE: sklearn
     print("Evaluating sklearn KDE")
-    for i in range(9, K):
-        k = i+1
-        print("\nk =", k)
-        kde = KDEClassifier(k)
+    for i, bandwidth in enumerate(ks):
+        print("bandwidth = ", bandwidth)
+        kde = KDEClassifier(bandwidth=bandwidth)
         kde.fit(X_train, Y_train)
 
         t0 = datetime.now()
-        train_score = kde.score(X_train, Y_train)
+        train_prob = kde.predict(X_train)
+        train_score = kde.eval_acc(train_prob, Y_train)
         train_scores.append(train_score)
         print("Train accuracy:", train_score)
         print("Time to compute train accuracy:", (datetime.now() - t0), "Train size:", len(Y_train))
 
         t0 = datetime.now()
-        test_score = knn.score(X_test, Y_test)
+        test_prob = kde.predict(X_test)
+        test_score = kde.eval_acc(test_prob, Y_test)
         print("Test accuracy:", test_score)
         test_scores.append(test_score)
         print("Time to compute test accuracy:", (datetime.now() - t0), "Test size:", len(Y_test))
@@ -52,20 +63,14 @@ if __name__ == '__main__':
         # update best accuracy (for comparison purpose later)
         if test_score > best_test_score:
             best_test_score = test_score
-            stored_accuracy = [test_score, k]
-
-        # np.savetxt(fname=csv_path + 'exec-knn-theano.csv', X=str("dsd"), delimiter=',', fmt='%d')
+            stored_accuracy = [test_score, bandwidth]
 
     fig = plt.figure()
     plt.plot(ks, train_scores, label='train scores')
     plt.plot(ks, test_scores, label='test scores')
     plt.legend()
     plt.show()
-    fig.savefig('hw5/results/result-knn-theano.png', dpi=fig.dpi)
-    save_to_csv('exec-knn-theano.csv', exec_times)
-    save_to_csv('knn-test-scores-theano.csv', test_scores)
-    save_to_csv('knn-best-acc-theano.csv', stored_accuracy)
-
-
-
-
+    fig.savefig('hw5/results/result-kde.png', dpi=fig.dpi)
+    save_to_csv('exec-kde.csv', exec_times)
+    save_to_csv('test-scores-kde.csv', test_scores)
+    save_to_csv('best-acc-kde.csv', stored_accuracy)
